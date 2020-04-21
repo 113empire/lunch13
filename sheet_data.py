57374a2d19430ccc220e52c745d726dd087e456e
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-04-21 20:18:41 +0800

    Update sheet_data.py

--- sheet_data.py
+++ sheet_data.py
@@ -9,7 +9,22 @@ gs = gspread.authorize(cr)
 
 sh = gs.open('lunch13')
 order_sheet = sh.worksheet('order')
+money_sheet = sh.worksheet('money')
+account_sheet = sh.worksheet('account')
+session_sheet = sh.worksheet('session')
 
-values = ['日期', '座號', '選購餐廳', '交易金額']
-order_sheet.insert_row(values, 1)
+order_values = ['日期', '座號', '選購餐廳', '交易金額']
+order_sheet.insert_row(order_values, 1)
 order_sheet.resize(1)
+
+money_values = ['學號', '座號', '餘額']
+money_sheet.insert_row(money_values, 1)
+money_sheet.resize(1)
+
+account_values = ['帳號', '密碼']
+account_sheet.insert_row(account_values, 1)
+account_sheet.resize(1)
+
+session_values = ['帳號', '密碼']
+session_sheet.insert_row(session_values, 1)
+session_sheet.resize(1)
