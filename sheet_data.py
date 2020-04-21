88fa8eed429fcc8e0a020ea314bf1299dff469e2
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-04-21 20:06:50 +0800

    Update sheet_data.py

--- sheet_data.py
+++ sheet_data.py
@@ -1,2 +1,15 @@
 import gspread
-#from auth2client import -----
+#from ... import ... as ...
+
+scope = ['https://spreadsheets.google.com/feeds', 
+         'https://www.googleapis.com/auth/drive']
+
+cr = sac.from_json_keyfile_name('model/google_auth.json', scope)
+gs = gspread.authorize(cr)
+
+sh = gs.open('lunch13')
+order_sheet = sh.worksheet('order')
+
+values = ['日期', '座號', '選購餐廳', '交易金額']
+order_sheet.insert_row(values, 1)
+order_sheet.resize(1)
