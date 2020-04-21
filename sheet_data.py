006836f3396a8a65e42764733ecb8538ef6433d6
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-04-21 21:52:12 +0800

    Update sheet_data.py

--- sheet_data.py
+++ sheet_data.py
@@ -15,20 +15,25 @@ session_sheet = sh.worksheet('session')
 
 order_values = ['日期', '座號', '選購餐廳', '交易金額']
 order_sheet.insert_row(order_values, 1)
-
+order_sheet.resize(1)
 
 money_values = ['學號', '座號', '餘額']
 money_sheet.insert_row(money_values, 1)
 
-money_sheet.insert_row([811406, 21, 0],2)
+for a in range(2,32):
+    li = [811385+a, a-1, 0]
+    money_sheet.insert_row(li, a)
+money_sheet.resize(31)
 
 
 account_values = ['帳號', '密碼']
 account_sheet.insert_row(account_values, 1)
+account_sheet.resize(1)
 
 
 session_values = ['session', 'cookie']
 session_sheet.insert_row(session_values, 1)
+session_sheet.resize(1)
 
 
 
