d66506b75cd71af5664661509c52c3a86b850c5a
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-04-21 20:20:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 from flask import Flask, render_template, request, redirect
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials as sac
 import sheet_data
 
 app = Flask(__name__)
 
-#
-scope = ['https://spreadsheets.google.com/feeds', 
-         'https://www.googleapis.com/auth/drive']
-
-cr = sac.from_json_keyfile_name('model/google_auth.json', scope)
-gs = gspread.authorize(cr)
-#
 
 @app.route('/')
 def index():
@@ -21,7 +12,7 @@ def index():
 def order():
     return render_template('order.html')
 
-@app.route('/order/process/', methods=['POST'])
+@app.route('/order_process/', methods=['POST'])
 def order_process():
     school_num = request.form.get('school_num')
     seat_num = request.form.get('seat_num')
@@ -33,20 +24,20 @@ def order_process():
     if response==False:
         return redirect('/order/money_not_enough/')
     
-    return redirect('/order/ok/')
+    return redirect('/order_successful')
 
-@app.route('/order/ok/')
+@app.route('/order_successful')
 def order_success():
-    return render_template('order_ok.html')
+    return render_template('order_successful.html')
 
 @app.route('/order/money_not_enough/')
 def order_money_not_enough():
     return render_template('money_not_enough.html')
-'''
+
 @app.route('/menu')
 def menu():
     return render_template('menu.html')
-
+'''
 @app.route('/management')
 def management():
     return render_templates('management.html')
