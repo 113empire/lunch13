a94027a16e8815da7120dea53286c08501f37c28
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-04-21 20:09:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials as sac
+import sheet_data
 
 app = Flask(__name__)
 
