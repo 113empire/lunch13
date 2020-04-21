6df575b5e093bda5eccce4c89fcd9c33fdff2d9d
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-04-21 20:42:03 +0800

    Update sheet_data.py

--- sheet_data.py
+++ sheet_data.py
@@ -21,6 +21,11 @@ money_values = ['學號', '座號', '餘額']
 money_sheet.insert_row(money_values, 1)
 money_sheet.resize(1)
 
+#
+money_values = [811406, 21, 0]
+money_sheet.insert_row(money_values, 2)
+#
+
 account_values = ['帳號', '密碼']
 account_sheet.insert_row(account_values, 1)
 account_sheet.resize(1)
@@ -28,3 +33,20 @@ account_sheet.resize(1)
 session_values = ['session', 'cookie']
 session_sheet.insert_row(session_values, 1)
 session_sheet.resize(1)
+
+
+def find(work_sheet, value):
+    for i in range( 1, len(work_sheet.col_values(1))+1 ):
+        if work_sheet.cell(i, 1)==work_sheet.cell(i, 1):
+            return i
+    return False
+
+def add_money(school_number, seat_number, how_much):
+    global money_sheet
+    index = find(money_sheet, school_number)
+    
+    if money_sheet.cell(i, 1)-811385==money_sheet.cell(i, 2):
+        money_sheet.update_cell(i, 3, money_sheet.cell(i, 3)+how_much)
+        
+    else:
+        return False
