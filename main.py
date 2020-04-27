4f472441a8d4e703a4b96c62e69444fdf6f50c13
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-04-27 14:44:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def order_process():
     
     #response = order_lunch(school_num, restaurant)
     
-'''
+    '''
     if response==False:
         return redirect('/order/money_not_enough/')
     '''
