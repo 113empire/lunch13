94e3955cf5eb90c43c740876881d44970cae674a
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-04-28 09:05:41 +0800

    Create else_function.py

---
+++ else_function.py
@@ -0,0 +1,12 @@
+#import ...
+#def get_wday(year, month, date):
+#    '''
+#    取得星期
+#    輸入值：
+#    year：字串，西元年份4位
+#    month：字串，月份2位
+#    date：字串，日期2位
+#    輸出值：
+#    w_day：字串，星期，一二三四五六日
+#    '''
+#    
