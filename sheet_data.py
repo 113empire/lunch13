0981bcba99a7499c4277a03b710be8878e861560
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-04-30 14:22:40 +0800

    Update sheet_data.py

--- sheet_data.py
+++ sheet_data.py
@@ -18,6 +18,37 @@ money_sheet = sh.worksheet('money')
 # money_values ['學號', '座號', '餘額']
 
 
+def check_money(school_number, seat_number, bigger_than):
+    '''
+    確認餘額是否足夠
+    輸入值：
+      school_number：學號，主要索引
+      seat_number：座號，確認學號
+      bigger_than：比較依據(餘額是否大於這個數)
+    回傳值：
+      True：夠
+      False：不夠
+      'wrong_number'：學號座號不符
+      'error'：表示其他錯誤
+    '''
+    global money_sheet
+
+    try:
+        cell = money_sheet.find(str(school_number)) #尋找學號
+        r = cell.row #學號的列(橫)
+        c = cell.col #學號的欄(直)
+
+        if money_sheet.cell(r, c+1).value==str(seat_number): #如果學號的右邊一格等於座號
+            result = int(money_sheet.cell(r, c+2).value)-int(bigger_than) >= 0 #把學號的右邊2格(原本的餘額)減去扣除金額是否大於0
+            return result
+        
+        else:
+            return 'wrong_number'
+        
+    except:
+        return 'error'
+
+
 def add_money(school_number, seat_number, how_much):
     '''
     增加金額(儲值)
@@ -58,7 +89,7 @@ def spend_money(school_number, seat_number, how_much):
       how_much：增加的金額
     回傳值：
       'wrong_number'：學號座號不符
-      False：錯誤，可能是找不到
+      'error'：錯誤，可能是找不到
       其他：字串，扣除後總金額
     '''
     global money_sheet
@@ -68,44 +99,13 @@ def spend_money(school_number, seat_number, how_much):
         r = cell.row #學號的列(橫)
         c = cell.col #學號的欄(直)
 
-        if money_sheet.cell(r, c+1).value==str(seat_number): #如果學號的右邊一格等於座號
+        if check_money(school_number, seat_number, how_much)==True: #如果學號的右邊一格等於座號
             total = int(money_sheet.cell(r, c+2).value)-int(how_much) #把學號的右邊2格(原本的餘額)減去扣除金額
             money_sheet.update_cell(r, c+2, str(total)) #更新學號右邊2格(餘額)
             return str(total)
         
         else:
-            return 'wrong_number'
-        
-    except:
-        return False
-
-    
-def check_money(school_number, seat_number, bigger_than):
-    '''
-    確認餘額是否足夠
-    輸入值：
-      school_number：學號，主要索引
-      seat_number：座號，確認學號
-      bigger_than：比較依據(餘額是否大於這個數)
-    回傳值：
-      True：夠
-      False：不夠
-      'wrong_number'：學號座號不符
-      'error'：表示其他錯誤
-    '''
-    global money_sheet
-
-    try:
-        cell = money_sheet.find(str(school_number)) #尋找學號
-        r = cell.row #學號的列(橫)
-        c = cell.col #學號的欄(直)
-
-        if money_sheet.cell(r, c+1).value==str(seat_number): #如果學號的右邊一格等於座號
-            result = int(money_sheet.cell(r, c+2).value)-int(bigger_than) >= 0 #把學號的右邊2格(原本的餘額)減去扣除金額是否大於0
-            return result
-        
-        else:
-            return 'wrong_number'
+            return check_money(school_number, seat_number, how_much)
         
     except:
         return 'error'
