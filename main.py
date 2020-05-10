1c80e6e73888c4a93d6ac3820ddab56275c149f5
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-05-10 20:12:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -77,9 +77,8 @@ def search_order_result():
     date = request.form.get('date')
     response = list(sheet_data.get_order_by_date(date))
     #以上OK
-    return response[2][0]
-    return render_template('search_order_result.html')#, date=response[0], total_price=response[1], \
-    #                       seat_num_list=response[2], restaurant_list=response[3], price_list=response[4])
+    return render_template('search_order_result.html', date=response[0], total_price=response[1], \
+                           seat_num_list=response[2], restaurant_list=response[3], price_list=response[4])
 
 @app.route('/update_menu')
 def update_menu_page():
