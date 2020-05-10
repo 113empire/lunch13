bef24fda252988a9b25dc2e51ef6a4de5435bca6
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-05-10 18:30:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,9 @@ def search_order():
 @app.route('/search_order_result', methods=['GET', 'POST'])
 def search_order_result():
     date = request.form.get('date')
+    return date#
     response = sheet_data.get_order_by_date(date)
+    return response#
     return render_template('search_order_result.html', date=response[0], total_price=response[1], \
                            seat_num_list=response[2], restaurant_list=response[3], price_list=response[4])
 
